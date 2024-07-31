fb5f6848dc9a66370b3a8a2eace562e3d8062fa8
Fyg369 <142138034+Fyg369@users.noreply.github.com>
2024-07-31 12:03:19 +0800

    Add files via upload

---
+++ tests/clean.pl
@@ -0,0 +1,19 @@
+#!/bin/perl -w
+
+use strict;
+use Cwd;
+use warnings;
+
+my $fileExist = -e "./.git";
+my $T = -e "./tests";
+
+if ( $fileExist ) {
+    system("git clean -dxf");
+    system("git checkout .");
+} else {
+    if ( $T ) {
+        unlink "tests/yarn.lock";
+        unlink "tests/shfmt*";
+        rmdir "tests/node_modules";
+    }
+}
