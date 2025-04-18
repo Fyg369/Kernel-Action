d088964ce3ab61acbf8149e85ceab3009d8a6099
Fyg369 <142138034+Fyg369@users.noreply.github.com>
2025-04-18 17:58:23 +0800

    Fix version number check

--- tests/patch.sh
+++ tests/patch.sh
@@ -131,7 +131,7 @@ int path_umount(struct path *path, int flags)\n\
 
     # security/selinux/hooks.c
     security/selinux/hooks.c)
-        if [ "$VERSION" -lt 5 ]; then
+        if grep -q "^VERSION = [1-4]" Makefile; then
         sed -i '/int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);/i\    static u32 ksu_sid;\n    char *secdata;' security/selinux/hooks.c
         sed -i '/if (!nnp && !nosuid)/i\    int error;\n    u32 seclen;\n' security/selinux/hooks.c
         sed -i '/return 0; \/\* No change in credentials \*\//a\\n    if (!ksu_sid)\n        security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);\n\n    error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);\n    if (!error) {\n        rc = strcmp("u:r:init:s0", secdata);\n        security_release_secctx(secdata, seclen);\n        if (rc == 0 && new_tsec->sid == ksu_sid)\n            return 0;\n    }' security/selinux/hooks.c
